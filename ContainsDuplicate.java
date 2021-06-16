65c691e30c714326900321297d14b81b858df59a
Snehal Mundhe <49169934+snehalmundhe10@users.noreply.github.com>
2021-06-16 12:39:08 -0400

    Update ContainsDuplicate.java

--- ContainsDuplicate.java
+++ ContainsDuplicate.java
@@ -1,1 +1,35 @@
+/*
+Given an integer array nums, return true if any value appears at least twice in the array, 
+and return false if every element is distinct.
+
+Example 1:
+Input: nums = [1,2,3,1]
+Output: true
+
+Example 2:
+Input: nums = [1,2,3,4]
+Output: false
+
+Example 3:
+Input: nums = [1,1,1,3,3,4,3,2,4,2]
+Output: true
+
+Constraints:
+1 <= nums.length <= 105
+-109 <= nums[i] <= 109
+*/
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+            int arrayLength = nums.length;
+            HashSet<Integer> set = new HashSet<>();
+            for(int i = 0 ; i < arrayLength; i++){
+              set.add(nums[i]);
+            }
+            int setSize = set.size();
+            if(setSize < arrayLength) return true;
+            else return false;
+    }
+      
+} 
 
